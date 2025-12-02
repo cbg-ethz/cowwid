fe92259c08598415b80160f3a05f609647d50299
Beerenwinkel TechnicalIdentity <bs-pangolin@eu-login-29.euler.ethz.ch>
2025-12-02 16:26:12 +0100

    [WIP] variable name correction

--- rsv_iva_heatmaps/scripts/run_iva_heatmap.sh
+++ rsv_iva_heatmaps/scripts/run_iva_heatmap.sh
@@ -7,11 +7,11 @@ conda activate iva_heatmap
 iva_input_path_N1="/cluster/project/pangolin/processes/influenza/IA_N1/working/mutation_frequencies/0_table_for_plotting/N1_Mutations.tsv"
 iva_input_path_N2="/cluster/project/pangolin/processes/influenza/IA_N2/working/mutation_frequencies/0_table_for_plotting/N2_Mutations.tsv"
 iva_input_path_H1="/cluster/project/pangolin/processes/influenza/IA_H1/working/mutation_frequencies/0_table_for_plotting/H1_Mutations.tsv"
-iva_input_path_H1="/cluster/project/pangolin/processes/influenza/IA_H3/working/mutation_frequencies/0_table_for_plotting/H3_Mutations.tsv"
+iva_input_path_H3="/cluster/project/pangolin/processes/influenza/IA_H3/working/mutation_frequencies/0_table_for_plotting/H3_Mutations.tsv"
 out_path="/cluster/project/pangolin/resources/cowwid/rsv_iva_heatmaps/results"
 
 ./iva_heatmapt.R \
-  -i "/${iva_input_path_N1},${iva_input_path_N2},${iva_input_path_H1},${iva_input_path_H1}" \
+  -i "/${iva_input_path_N1},${iva_input_path_N2},${iva_input_path_H1},${iva_input_path_H3}" \
   -o "${out_path}" \
   -r "/cluster/project/pangolin/resources/cowwid/rsv_iva_heatmaps/resources/N1_resistanceMuts.tsv"
 
