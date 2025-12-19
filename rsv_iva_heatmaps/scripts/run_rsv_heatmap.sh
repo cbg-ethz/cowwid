572fd3f83f8d85f327f1837b51eef4dbce16181c
Beerenwinkel TechnicalIdentity <bs-pangolin@eu-login-19.euler.ethz.ch>
2025-12-19 14:55:27 +0100

    [WIP] added detection of the latest batch

--- rsv_iva_heatmaps/scripts/run_rsv_heatmap.sh
+++ rsv_iva_heatmaps/scripts/run_rsv_heatmap.sh
@@ -4,7 +4,17 @@ set -euo pipefail
 eval "$(/cluster/project/pangolin/resources/miniconda3/bin/conda shell.bash hook)"
 conda activate rsv_heatmap
 
-current_batch="20251128_2511665243"
+#current_batch="20251128_2511665243"
+current_batch=$(
+  find /cluster/project/pangolin/processes/rsv/RSVA/working/MutationFrequencies \
+    -name '*_EPI_ISL_412866_Mutations_Dashboard.tsv' \
+    -printf '%f\n' \
+  | sed 's/_EPI_ISL_412866_Mutations_Dashboard.tsv$//' \
+  | sort -V \
+  | tail -n 1
+)
+echo "Processing batch:"
+echo ${current_batch}
 mut_file_A="/cluster/project/pangolin/processes/rsv/RSVA/working/MutationFrequencies/${current_batch}_EPI_ISL_412866_Mutations_Dashboard.tsv"
 mut_file_B="/cluster/project/pangolin/processes/rsv/RSVB/working/MutationFrequencies/${current_batch}_EPI_ISL_1653999_Mutations_Dashboard.tsv"
 out_path="/cluster/project/pangolin/resources/cowwid/rsv_iva_heatmaps/results_rsv"
