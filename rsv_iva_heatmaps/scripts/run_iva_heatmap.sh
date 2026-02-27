aed6fe6279ec950775be75b25a5ffa709fbb4f34
Beerenwinkel TechnicalIdentity <bs-pangolin@eu-login-05.euler.ethz.ch>
2026-02-27 10:53:44 +0100

    [CONFIG] improved results directory name

--- rsv_iva_heatmaps/scripts/run_iva_heatmap.sh
+++ rsv_iva_heatmaps/scripts/run_iva_heatmap.sh
@@ -8,7 +8,8 @@ iva_input_path_N1="/cluster/project/pangolin/processes/influenza/IA_N1/working/m
 iva_input_path_N2="/cluster/project/pangolin/processes/influenza/IA_N2/working/mutation_frequencies/0_table_for_plotting/N2_Mutations.tsv"
 iva_input_path_H1="/cluster/project/pangolin/processes/influenza/IA_H1/working/mutation_frequencies/0_table_for_plotting/H1_Mutations.tsv"
 iva_input_path_H3="/cluster/project/pangolin/processes/influenza/IA_H3/working/mutation_frequencies/0_table_for_plotting/H3_Mutations.tsv"
-out_path="/cluster/project/pangolin/resources/cowwid/rsv_iva_heatmaps/results"
+out_path="/cluster/project/pangolin/resources/cowwid/rsv_iva_heatmaps/results_iva"
+mkdir -p $out_path
 
 ./iva_heatmapt.R \
   -i "/${iva_input_path_N1},${iva_input_path_N2},${iva_input_path_H1},${iva_input_path_H3}" \
